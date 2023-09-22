5022a5449d9d751505a17339509e89bd58d16833
Demand Btc <102271199+Demandtech@users.noreply.github.com>
2023-09-22 08:29:13 +0100

    fix: cors

--- server/settings.py
+++ server/settings.py
@@ -184,6 +184,7 @@ EMAIL_PORT = 587
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
+    'http://localhost:8000',
     'http://127.0.0.1:3000',
     'https://ara-shop.vercel.app',
     # Add other origins if needed
